ca10cd1c259721bb15ad91f487c31c86c7066b6f
Andrey Gromozdov <1977andy02@mail.ru>
2013-12-08 20:33:22 +0400

    install service rewrite

--- scripts/install.js
+++ scripts/install.js
@@ -7,20 +7,21 @@
 var platform = require('os').platform();
 var Service;
 
-if (/win/.test(platform)) {
-    console.log('install windows service');
-    Service = require('../lib/node-windows').Service;
+
+if (/win32/.test(platform)) {
+    console.log('install windows service...');
+    Service = require('../lib/service/windows').Service;
 }
 
-if (/inux/.test(platform)) {
-    console.log('install linux daemon');
-    Service = require('node-linux').Service;
+if (/linux/.test(platform)) {
+    console.log('install linux daemon...');
+    Service = require('../lib/service/systemv/index').Service;
 }
 
 
 // Create a new service object
 var svc = new Service({
-    name: 'node-hosting',
+    name: 'nodehosting',
     description: 'The node.js deploy service',
     script: './lib/server.js'
 });
@@ -28,7 +29,7 @@ var svc = new Service({
 // Listen for the "install" event, which indicates the
 // process is available as a service.
 svc.on('install',function(){
-    process.exit(0);
+    console.log('service installed.');
 });
 
 svc.install();
